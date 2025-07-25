04c02a0527421e565866e56b13b09dbb19156913
Your Name <you@example.com>
2025-07-25 15:43:15 +0530

    sample

---
+++ python _again_start_learning_2025/python_practise/python_db_connection/python_database_connectivity.py
@@ -0,0 +1,15 @@
+# import pymysql
+# import pymysql.cursors
+
+# # connection =pymysql.connect(
+#     host="localhost",
+#     user="root",
+#     password="root",
+#     database="test",
+#     # cursorclass=pymysql.cursors.DictCursor
+    # )
+# try:
+
+
+
+                                              
